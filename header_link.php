e33da6de4d61d2d48b7d627e5aa28d343b1f0ad0
dhruvdddd <dhruvbhimani2222@gmail.com>
2024-04-24 19:17:12 +0530

    Initial commit

---
+++ header_link.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/style.css">
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="script/script.js"></script>
